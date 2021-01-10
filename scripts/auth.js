6378bc623bb19a6a336c034323e90ff90c37c5de
Goldentoaste <a2375658@gmail.com>
2021-01-10 01:42:06 -0800

    stuff

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,29 +1,51 @@
 
 
-var provider = new firebase.auth.GoogleAuthProvider();
-
-//sign up
 
 
 
 
 const signupButton = document.getElementById("signup-button");
 
-signupButton.addEventListener("click", e => {
+if (signupButton != null){
+    signupButton.addEventListener("click", e => {
 
-    //get info 
-    const pass = document.getElementById("signup-password").value;
-    const email = document.getElementById("signup-email").value;
-    const name = document.getElementById("signup-name").value;
-
-    //sign up user
-    auth.createUserWithEmailAndPassword(email, pass).then(cred => {
-        cred.user.updateProfile({displayName:name});
-        //redir to main page
-        window.location.href ="app.html";
-    }).catch(err =>{
-            window.alert(`${err.message}"`);
+        //get info 
+        const pass = document.getElementById("signup-password").value;
+        const email = document.getElementById("signup-email").value;
+        const name = document.getElementById("signup-name").value;
+    
+        //sign up user
+        auth.createUserWithEmailAndPassword(email, pass).then(cred => {
+            cred.user.updateProfile({displayName:name});
+            //redir to main page
+            window.location.href ="project.html";
+        }).catch(err =>{
+                window.alert(`${err.message}"`);
+        });
     });
-});
+    
+}
 
+const loginButton = document.getElementById('login-button');
+if (loginButton != null){
+    loginButton.addEventListener("click", e => {
+        const pass = document.getElementById("login-password").value;
+        const email = document.getElementById("login-email").value;
+        console.log('entered event');
+        auth.signInWithEmailAndPassword(email, pass).then(cred =>{
+            console.log(cred.user);
+            window.location.href ="graphTestings.html";
+        }).catch(err=>{
+                console.log(err.message)
+            });
+    })
+    
+}
 
+const test = document.getElementById('test-button');
+
+if (test != null){
+    test.addEventListener("click", e=>{
+        console.log(auth.currentUser);
+    });
+}
