abde103d668c7db65e6a4cc5c115387a7e746987
Goldentoaste <a2375658@gmail.com>
2021-01-09 18:33:01 -0800

    signup with email

--- scripts/auth.js
+++ scripts/auth.js
@@ -3,6 +3,26 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 
 //sign up
-const googleButton = document.querySelector("googleButton");
 
-googleButton.addEventListener("click",)
+
+
+
+const signupButton = document.getElementById("signup-button");
+
+signupButton.addEventListener("click", e => {
+
+    //get info 
+    const pass = document.getElementById("signup-password").value;
+    const email = document.getElementById("signup-email").value;
+
+    //sign up user
+    auth.createUserWithEmailAndPassword(email, pass).then(cred => {
+        console.log(cred.user);
+        
+        //redir to main page
+    }).catch(err =>{
+            window.alert(`${err.message}"`);
+    });
+});
+
+
